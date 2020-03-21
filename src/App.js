8b3b8eccf6bcf5398f7ac1f0e171983f2e0ec60b
Eduardo Freitas <edusf8700@gmail.com>
2020-03-20 22:00:38 -0300

    Criado o estilo global

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import GlobolStyle from './styles/global';
+import Routes from './routes';
 
 function App() {
-  return <h1>EDU</h1>;
+  return (
+    <BrowserRouter>
+      <Routes />
+      <GlobolStyle />
+    </BrowserRouter>
+  );
 }
 
 export default App;
